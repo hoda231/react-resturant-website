6bae21a4e119b85b3634852f4015ed1c2f5fb3d1
hoda231 <hoda36388@gmail.com>
2026-01-04 06:21:16 +0200

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,45 @@
 import React from "react";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { FaFacebookF } from "react-icons/fa";
 import "../styles/Footer.css";
+import { FaYoutube } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="socialMedia">
-        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
-      </div>
-      <p> &copy; 2021 pedrotechpizza.com</p>
-    </div>
+    <footer class="footer">
+		<div class="container">
+			<div class="row">
+			<div class="footer-col">
+				<h4>get help</h4>
+
+             <ul>
+
+             <li><a href="#">FAQ</a></li>
+			 <li><a href="#">shipping</a></li>
+			 <li><a href="#">returns</a></li>
+			 <li><a href="#">order status</a></li>
+			 <li><a href="#">payment options</a></li>
+			</ul>
+		</div>
+		<div class="footer-col">
+			<h4>online shop</h4>
+			<ul>
+				<li><a href="#">pizza</a></li>
+			
+			
+			</ul>
+		</div>
+		<div class="footer-col">
+			<h4>follow us</h4>
+			<div class="social-links">
+			 <a> <FaFacebookF  className="bag-icon" /></a>
+					<a ><FaYoutube /></a>
+					<a href="#"><FaInstagram /></a>
+			</div>
+			</div>
+			</div>
+		</div>
+	</footer>
   );
 }
 
